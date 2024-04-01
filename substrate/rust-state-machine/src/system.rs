2349bc5f6f13c329a8faa5b05fd3f37d518c54ec
Gerson Loaiza Vásquez <gersonloavas@gmail.com>
2024-04-01 10:45:06 -0600

    implementing new functions

--- substrate/rust-state-machine/src/system.rs
+++ substrate/rust-state-machine/src/system.rs
@@ -21,4 +21,38 @@ impl Pallet {
             nonce: BTreeMap::new(),
         }
     }
+
+    /// Get the current block number.
+    pub fn block_number(&self) -> u32 {
+        return self.block_number;
+    }
+
+    // This function can be used to increment the block number.
+    // Increases the block number by one.
+    pub fn inc_block_number(&mut self) {
+        /* TODO: Increment the current block number by one. */
+        self.block_number += 1;
+    }
+
+    // Increment the nonce of an account. This helps us keep track of how many transactions each
+    // account has made.
+    pub fn inc_nonce(&mut self, who: &String) {
+        /* TODO: Get the current nonce of `who`, and increment it by one. */
+        let nonce = *self.nonce.get(who).unwrap_or(&0);
+        let new_nonce = nonce + 1;
+        self.nonce.insert(who.clone(), new_nonce);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn init_system() {
+        /* TODO: Create a test which checks the following:
+            - Increment the current block number.
+            - Increment the nonce of `alice`.
+            - Check the block number is what we expect.
+            - Check the nonce of `alice` is what we expect.
+        */
+    }
 }
